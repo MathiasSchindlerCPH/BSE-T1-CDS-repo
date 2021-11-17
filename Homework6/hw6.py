76c02c84bc241238eb53ebac928a099ff4a608c0
MathiasSchindlerCPH <mathingvid@gmail.com>
2021-11-17 10:06:37 +0100

    Exc3.2 added (my version)

--- Homework6/hw6.py
+++ Homework6/hw6.py
@@ -204,6 +204,26 @@ class PlaneFigure(ABC):
         pass
 
 # 3.2 Create a child class called "Triangle" that inherits from "PlaneFigure" and has as parameters in the constructor "base", "c1", "c2", "h". ("base" being the base, "c1" and "c2" the other two sides of the triangle and "h" the height). Implement the abstract methods with the formula of the triangle.
+class Triangle(PlaneFigure):
+    def __init__(self, base, c1, c2, h):
+        self.base = base
+        self.c1 = c1
+        self.c2 = c2
+        self.h = h
+    
+    def compute_perimeter(self):
+        return self.base + self.c1 + self.c2
+    
+    def compute_surface(self):
+        return 0.5*self.base*self.h
+
+
+#testing:
+test = Triangle(2, 1, 0.75, 0.2)
+test.compute_perimeter()
+test.compute_surface()
+
+
 
 # 3.3 Create a child class called "Rectangle" that inherits from "PlaneFigure" and has as parameters in the constructor "a", "b" (sides of the rectangle). Implement the abstract methods with the formula of the rectangle.
 
