61eb0d4511db8a2fa52dd4bddae1d6dbbcdf63be
andresvbse <andres.virguez@bse.eu>
2021-12-01 12:31:28 +0100

    Small changes in the assert line

--- Homework5/test/test_hw.py
+++ Homework5/test/test_hw.py
@@ -5,7 +5,7 @@ import unittest
 import sys
 sys.path.append('../')
 
-from functions import car_at_light, safe_subtract, retrieve_age_eafp, get_day_month_year, compute_distance, sum_general_int_list
+from functions import get_day_month_year, compute_distance, sum_general_int_list, car_at_light, safe_subtract, retrieve_age_eafp, retrieve_age_lbyl
 
 from pandas.testing import assert_frame_equal
 
@@ -20,19 +20,19 @@ class TestHW5(unittest.TestCase):
         x = 'red'
         output = car_at_light(x)
         expected_output = 'stop'
-        assert output == expected_output
+        self.assertEqual(output, expected_output)
         
     def test_car_at_light_2(self):
         x = 'yellow'
         output = car_at_light(x)
         expected_output = 'wait'
-        assert output == expected_output
+        self.assertEqual(output, expected_output)
         
     def test_car_at_light_3(self):
         x = 'green'
         output = car_at_light(x)
         expected_output = 'go'
-        assert output == expected_output
+        self.assertEqual(output, expected_output)
         
     #2
     def test_safe_subtract_1(self):
@@ -40,7 +40,7 @@ class TestHW5(unittest.TestCase):
         x2 = 3
         output = safe_subtract(x1, x2)
         expected_output = 2
-        assert output == expected_output
+        self.assertEqual(output, expected_output)
         
         
     def test_safe_subtract_2(self):
@@ -48,21 +48,21 @@ class TestHW5(unittest.TestCase):
         x2 = 30
         output = safe_subtract(x1, x2)
         expected_output = -20
-        assert output == expected_output
+        self.assertEqual(output, expected_output)
     
     #3
     def test_retrieve_age_eafp():
         x = {'name': 'Meryl', 'last_name': 'Streep', 'birth': 1949}
         output = retrieve_age_eafp(x)
         expected_output = 'Meryl Streep is 72 years old'
-        assert output == expected_output
+        self.assertEqual(output, expected_output)
     
     
     def test_retrieve_age_lbyl():
         x = {'name': 'Will', 'last_name': 'Smith', 'birth': 1968}
         output = retrieve_age_lbyl(x)
         expected_output = 'Will Smith is 53 years old'
-        assert output == expected_output
+        self.assertEqual(output, expected_output)
         
     #4
     
@@ -93,12 +93,5 @@ class TestHW5(unittest.TestCase):
         output = sum_general_int_list(data)
         expected_output = 48
         self.assertEqual(output, expected_output)
-
-
-
-#list_of_dates = [date(1996, 12, 11), date(1992, 2, 1), date(2021, 11, 11)]
-
-#df_dates = pd.DataFrame(map(get_day_month_year, list_of_dates))
-#print(df_dates)
 
 
